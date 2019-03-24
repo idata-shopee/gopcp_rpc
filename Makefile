c05adcb817a16e81c6ea31f54ee4415c5dc6f571
loveKino <842914439@qq.com>
2019-03-24 22:56:52 +0800

    add simple pcp cli tool

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ export GOPATH
 test:
 	@go test -v -race
 
+build-mac:
+	@cd tool && GOOS=darwin GOARCH=amd64 go build -o ../bin/mac/pcp-cli
+
 cover:
 	@go test -coverprofile=coverage.out
 	@go tool cover -html=coverage.out
---
+++ tool/pcp-cli.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	rpc "github.com/idata-shopee/gopcp_rpc"
+	"time"
+)
+
+func main() {
+	host := flag.String("host", "127.0.0.1", "host for pcp server")
+	port := flag.Int("port", 4231, "port for pcp server")
+	timeout := flag.Int("timeout", 300, "timeout for request")
+	text := flag.String("code", "[\"List\", \"hello\"]", "code")
+
+	flag.Parse()
+
+	// create client
+	if client, err := rpc.GetPCPRPCClient(*host, *port, func(e error) {}); err != nil {
+		panic(err)
+	} else {
+		if ret, err := client.CallRemote(*text, time.Duration(*timeout)*time.Second); err != nil {
+			panic(err)
+		} else {
+			fmt.Printf("%v", ret)
+			client.Close()
+		}
+	}
+}
